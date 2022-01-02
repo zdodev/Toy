42f51418deff46a2745ecbf84b958e7474a5f88e
zdodev <iver0945@gmail.com>
2022-01-02 11:41:32 +0900

    ✨: 티커 입력

--- AlphaVantageCommand/AlphaVantageCommand/main.swift
+++ AlphaVantageCommand/AlphaVantageCommand/main.swift
@@ -1,11 +1,29 @@
-//
-//  main.swift
-//  AlphaVantageCommand
-//
-//  Created by Zero DotOne on 2022/01/01.
-//
-
 import Foundation
 
-print("Hello, World!")
+print("ticker: ", terminator: "")
+let ticker = readLine()!
+let url = URL(string: "https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=\(ticker)&apikey=WIN4ZCY6VUPIHEB9")!
 
+let p = URLSession.shared
+    .dataTaskPublisher(for: url)
+    .sink { error in
+    switch error {
+    case .finished:
+        print("finished")
+    case .failure(let error):
+        print(error)
+    }
+} receiveValue: { data in
+    if let json = try? JSONSerialization.jsonObject(with: data.data, options: []) as? [String: Any] {
+        if let a = json["Monthly Adjusted Time Series"] as? [String: Any] {
+            a.keys.sorted().forEach { date in
+                if let b = a[date] as? [String: String] {
+                    print(date, b["7. dividend amount"]!)
+                }
+            }
+        }
+    }
+}
+
+
+RunLoop.main.run()
