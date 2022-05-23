5156a40b270634f8c6544fc447baff45332c67bf
zdodev <iver0945@gmail.com>
2022-05-23 16:27:29 +0900

    ✨: main 로직 변경

--- AlphaVantageCommand/AlphaVantageCommand/main.swift
+++ AlphaVantageCommand/AlphaVantageCommand/main.swift
@@ -3,9 +3,21 @@ import Combine
 
 typealias Ticker = String
 private var cancellables = Set<AnyCancellable>()
+
+struct View {
+    let date: String
+    let dividend: String
+}
+
+var views = [View]() {
+    didSet {
+        print(views)
+    }
+}
+
 let url = "https://www.alphavantage.co"
 let a = AlphaVantageWebService(session: URLSession.shared, baseURL: url)
-a.loadDividends("AAPL")
+a.loadDividends("BST")
     .sink { result in
         switch result {
         case .failure(let error):
@@ -14,36 +26,14 @@ a.loadDividends("AAPL")
             print("finished")
         }
     } receiveValue: { value in
-        print(value)
+        views = value.monthly.stockDatas
+            .sorted { lhs, rhs in
+                lhs.date < rhs.date
+            }
+            .map { stockData in
+            View(date: stockData.date, dividend: stockData.dividend)
+        }
     }
     .store(in: &cancellables)
-
-
-//
-//print("ticker: ", terminator: "")
-//let ticker = readLine()!
-//let url = URL(string: "https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=\(ticker)&apikey=WIN4ZCY6VUPIHEB9")!
-//
-//let p = URLSession.shared
-//    .dataTaskPublisher(for: url)
-//    .sink { error in
-//    switch error {
-//    case .finished:
-//        print("finished")
-//    case .failure(let error):
-//        print(error)
-//    }
-//} receiveValue: { data in
-//    if let json = try? JSONSerialization.jsonObject(with: data.data, options: []) as? [String: Any] {
-//        if let a = json["Monthly Adjusted Time Series"] as? [String: Any] {
-//            a.keys.sorted().forEach { date in
-//                if let b = a[date] as? [String: String] {
-//                    print(date, b["7. dividend amount"]!)
-//                }
-//            }
-//        }
-//    }
-//}
-
 
 RunLoop.main.run()
